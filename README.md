7ab803965f67766516176a96fc2e442058afac69
Lukas <lukas.dennert@campus.lmu.de>
2022-05-17 17:35:44 +0100

    pagerank working, readme updated

--- README.md
+++ README.md
@@ -16,3 +16,23 @@ personal implementation of a Erdos-Renyi model to generate a random graph
 
 ## Barabasi-Albert
 personal implementation of a Erdos-Renyi model to generate a random graph
+
+
+# PageRank 
+personal implementation of PageRank with power iteration and random teleportation
+
+## Usage
+To run both the PageRank on a predefined network with 7 nodes, just run the following command
+```
+python page_rank.py
+```
+
+
+Three additional parameter can be set via arguments.  
+The amount of iterations (-i), the dampening factor / beta (-b)  
+and a verbose (-v) mode to get informative prints on the command line.  
+```
+python page_rank.py -i 10 -b 0.73 -v
+```
+
+
--- page_rank.py
+++ page_rank.py
@@ -1,23 +1,43 @@
-import random
+import argparse
 
 class Node:
     def __init__(self, value):
         # no checks for the value or anything, everything is force-parsed into a string
         self.value = str(value)
-        self.edges = []
+        self.children = []
+        self.parents = []
+        self.pagerank = 1.0
 
     def __repr__(self):
         return self.value
 
     def add_edge(self, node):
         # only adds the edge to the list if it is not there already
-        if node not in self.edges:
-            self.edges.append(node)
+        if node not in self.children:
+            self.children.append(node)
+        if self not in node.parents:
+            node.parents.append(self)
     
     def remove_edge(self, node):
         # only removes the edge if it is in the list
-        if node in self.edges:
-            self.edges.remove(node)
+        if node in self.children:
+            self.children.remove(node)
+        if self in node.parents:
+            node.parents.remove(self)
+
+    def calculate_pagerank(self, beta, num_all_nodes, iteration, verbose):
+        """
+        Inspiration taken from https://towardsdatascience.com/pagerank-3c568a7d2332
+        """
+        importance = 0
+        for parent in self.parents:
+            importance += parent.pagerank / len(parent.children)
+        random_teleport = (1-beta) / num_all_nodes
+        rank = random_teleport + beta*importance
+
+        if verbose:
+            print("Node " + str(self.value) + ": " + str("{:10.8f}".format(rank)) + " at iteration " + str(iteration))
+        self.pagerank = rank
 
 
 class Manual_Network():
@@ -27,25 +47,25 @@ class Manual_Network():
 
     def add_node(self, node):
         self.nodes.append(node)
-        for edge_out in node.edges:
+        for edge_out in node.children:
             self.edges.append((node, edge_out))
 
-    def calculate_pagerank(self, verbose):
-        # initialize with 1/n
-        ranks = [1/self.get_size()] * self.get_size()
-        iteration_counter = 0
-        meaningful_changes = True
-        while meaningful_changes:
-            # TODO actual PageRank
-            iteration_counter += 1
-            if True: #TODO some other criteria
-                meaningful_changes = False
-            if verbose:
-                print("iteration:", iteration_counter)
-                for node, rank in zip(self.nodes, ranks):
-                    print(node, rank)
-                print("\n")
-                
+    def iterate_pagerank(self, beta, iterations, verbose):
+        """
+        Inspiration taken from https://towardsdatascience.com/pagerank-3c568a7d2332
+        """
+        for iteration in range(iterations):
+            for node in self.nodes:
+                node.calculate_pagerank(beta, self.get_size(), iteration+1, verbose)
+            self.normalize_pagerank()
+
+    def normalize_pagerank(self):
+        """
+        Inspiration taken from https://towardsdatascience.com/pagerank-3c568a7d2332
+        """
+        pagerank_sum = sum(node.pagerank for node in self.nodes)
+        for node in self.nodes:
+            node.pagerank /= pagerank_sum
 
     def get_size(self):
         return len(self.nodes)
@@ -56,6 +76,10 @@ class Manual_Network():
     def print_network(self):
         print("Nodes", len(self.nodes), self.nodes)
         print("Edges", len(self.edges), self.edges)
+        page_rank_string = ""
+        for node in self.nodes:
+            page_rank_string += str(node.value) + ": " + str(node.pagerank) + "; "
+        print("PageRank Selfmade:", page_rank_string[:-2])
 
     def store_network_as_txt_file(self, filename):
         with open(filename, 'w') as f:
@@ -67,6 +91,7 @@ class Manual_Network():
 def create_pagerank_network():
     # create empty network
     network = Manual_Network()
+
     # create nodes
     node_A = Node("A")
     node_B = Node("B")
@@ -100,14 +125,30 @@ def create_pagerank_network():
     return network
 
 
-def main():
-    # -------------------------------------------------------
-
+def main(args):
     # test parameter for the PageRank model
     pagerank_network = create_pagerank_network()
-    print("# of edges", pagerank_network.get_num_edges())
+    print("# of edges", pagerank_network.get_num_edges())    
+    pagerank_network.iterate_pagerank(beta=args.beta, iterations=args.iterations, verbose=args.verbose)
     pagerank_network.print_network()
-    pagerank_network.calculate_pagerank(verbose=True)
+
+
+def range_limited_float_type(arg):
+    """
+    Taken from https://stackoverflow.com/a/55410582
+    Type function for argparse - a float within some predefined bounds
+    """
+    try:
+        f = float(arg)
+    except ValueError:    
+        raise argparse.ArgumentTypeError("Must be a floating point number")
+    if f < 0.0 or f > 1.0:
+        raise argparse.ArgumentTypeError("Argument must be < " + str(1.0) + "and > " + str(0.0))
+    return f
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--iterations', type=int, default=5, help='iterations of pagerank')
+    parser.add_argument('-b', '--beta', type=range_limited_float_type, default=0.85, help='dampening factor for pagerank')
+    parser.add_argument('-v', '--verbose', action='store_true', help='toggle informative prints')
+    main(parser.parse_args())
